265ff5b4b0d84edcff8441f5249636c783ab9f97
Firdaus Suhuyini Fuseini <61602470+Suhuyini94@users.noreply.github.com>
2021-10-31 01:16:01 +0000

    First Commit

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignInScreen from './screens/SignInScreen';
+import SignUpScreen from './screens/SignUpScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <NavigationContainer>
+         <Stack.Navigator screenOptions = {{header:()=>null}}>
+            <Stack.Screen name = "Home" component = {SignInScreen} />
+            <Stack.Screen name = "SignUP" component = {SignUpScreen} />
+         </Stack.Navigator>
+      </NavigationContainer>
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
